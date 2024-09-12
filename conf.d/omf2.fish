543017eb9003bc7265cbf7db20c1d3cec595ca39
mattmc3 <mattmc3@gmail.com>
2024-09-12 10:17:58 -0400

    Update events

--- conf.d/omf2.fish
+++ conf.d/omf2.fish
@@ -1,20 +1,16 @@
-set --query omf2_path || set -gx omf2_path $__fish_config_dir/.omf2
+set --query omf2_path || set --universal omf2_path $__fish_config_dir/.omf2
 
-# Support for using Fisher to install the 'omf2' utility.
+# Support for using Fisher to install 'omf2'.
 function _omf2_install --on-event omf2_install
-    test "$__omf2_fisher_events_disabled" != true || return 0
-    if test -d $omf2_path
-        rm -rf -- $omf2_path
-    end
+    test -d $omf2_path && return 1
     git clone --quiet https://github.com/oh-my-fish-2/omf2 $omf2_path
 end
 
 function _omf2_update --on-event omf2_update
-    test "$__omf2_fisher_events_disabled" != true || return 0
     git -C $omf2_path pull --quiet
 end
 
 function _omf2_uninstall --on-event omf2_uninstall
-    test "$__omf2_fisher_events_disabled" != true || return 0
-    rm -rf -- $omf2_path
+    # TODO - maybe we want to be careful here... fisher uninstall contribs, etc.
+    # rm -rf -- $omf2_path
 end
