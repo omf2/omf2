0882e89dc6a0e8ce2e787009fde627c5d074d349
mattmc3 <mattmc3@gmail.com>
2024-09-18 07:41:08 -0400

    Use omf2 github

--- conf.d/omf2.fish
+++ conf.d/omf2.fish
@@ -3,7 +3,7 @@ set --query omf2_path || set --universal omf2_path $__fish_config_dir/.omf2
 # Support for using Fisher to install 'omf2'.
 function _omf2_install --on-event omf2_install
     test -d $omf2_path && return
-    git clone --quiet https://github.com/oh-my-fish-2/omf2 $omf2_path
+    git clone --quiet https://github.com/omf2/omf2 $omf2_path
 end
 
 function _omf2_update --on-event omf2_update
@@ -12,5 +12,6 @@ end
 
 function _omf2_uninstall --on-event omf2_uninstall
     # TODO - maybe we want to be careful here... fisher uninstall plugin packs, etc.
+    # omf2 disable --all
     rm -rf -- $omf2_path
 end
