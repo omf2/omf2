8d95c9ddc1d0330e65d170f6a06f577797e17745
mattmc3 <mattmc3@gmail.com>
2024-09-17 22:25:39 -0400

    Update omf2

--- conf.d/omf2.fish
+++ conf.d/omf2.fish
@@ -3,11 +3,11 @@ set --query omf2_path || set --universal omf2_path $__fish_config_dir/.omf2
 # Support for using Fisher to install 'omf2'.
 function _omf2_install --on-event omf2_install
     test -d $omf2_path/oh-my-fish-2/omf2 && return 1
-    git clone --quiet https://github.com/oh-my-fish-2/omf2 $omf2_path/oh-my-fish-2/omf2
+    git clone --quiet https://github.com/oh-my-fish-2/omf2 $omf2_path
 end
 
 function _omf2_update --on-event omf2_update
-    git -C $omf2_path/oh-my-fish-2/omf2 pull --quiet
+    git -C $omf2_path pull --quiet
 end
 
 function _omf2_uninstall --on-event omf2_uninstall
