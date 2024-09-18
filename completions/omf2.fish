4a3a274d16af747586f7c98bbbd256bb93a0b9fb
mattmc3 <mattmc3@gmail.com>
2024-09-17 22:55:50 -0400

    Update completions

--- completions/omf2.fish
+++ completions/omf2.fish
@@ -1,5 +1,6 @@
 complete --command omf2 --short-option h --long-option help --description "Print Oh-My-Fish-2 help"
 complete --command omf2 --short-option v --long-option version --description "Print Oh-My-Fish-2 version"
+complete --command omf2 --no-files --exclusive --condition __fish_use_subcommand --arguments list --description "List available OMF2 plugins"
 complete --command omf2 --no-files --exclusive --condition __fish_use_subcommand --arguments enable --description "Use an OMF2 plugin"
 complete --command omf2 --no-files --exclusive --condition __fish_use_subcommand --arguments disable --description "Stop using an OMF2 plugin"
 complete --command omf2 --no-files --exclusive --condition "__fish_seen_subcommand_from enable disable" --arguments "(omf2 list)"
